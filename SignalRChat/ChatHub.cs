df13295cba769203b9fc048b7d8a25aae115229d
brucechen1019 <lin.chen1019@hotmail.com>
2017-07-27 11:17:15 +0800

    init

---
+++ SignalRChat/ChatHub.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        private readonly static ConnectionMapping<string> _connections =
+            new ConnectionMapping<string>();
+
+        public void SendChatMessage(string senderName, string receiverName, string message)
+        {
+            _connections.Add(senderName, Context.ConnectionId);
+
+            if (string.IsNullOrEmpty(receiverName))
+                Clients.All.broadcastMessage(senderName, message);
+            else
+            {
+                foreach (var connectionId in _connections.GetConnections(receiverName))
+                {
+                    Clients.Client(connectionId).addChatMessage(senderName, message);
+                }
+            }
+            
+        }
+
+        public override Task OnConnected()
+        {
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected()
+        {
+            _connections.RemoveByConnectionId(Context.ConnectionId);
+            return base.OnDisconnected();
+        }
+
+        public override Task OnReconnected()
+        {
+            string name = Context.User.Identity.Name;
+
+            if (!_connections.GetConnections(name).Contains(Context.ConnectionId))
+            {
+                _connections.Add(name, Context.ConnectionId);
+            }
+
+            return base.OnReconnected();
+        }
+    }
+}
---
+++ SignalRChat/ConnectionMapping.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRChat
+{
+    public class ConnectionMapping<T>
+    {
+        private readonly Dictionary<T, HashSet<string>> _connections =
+            new Dictionary<T, HashSet<string>>();
+
+        public int Count
+        {
+            get
+            {
+                return _connections.Count;
+            }
+        }
+
+        public void Add(T key, string connectionId)
+        {
+            lock (_connections)
+            {
+                HashSet<string> connections;
+                if (!_connections.TryGetValue(key, out connections))
+                {
+                    connections = new HashSet<string>();
+                    _connections.Add(key, connections);
+                }
+
+                lock (connections)
+                {
+                    if (!connections.Contains(connectionId))
+                        connections.Add(connectionId);
+                }
+            }
+        }
+
+        public IEnumerable<string> GetConnections(T key)
+        {
+            HashSet<string> connections;
+            if (_connections.TryGetValue(key, out connections))
+            {
+                return connections;
+            }
+
+            return Enumerable.Empty<string>();
+        }
+
+        public void Remove(T key, string connectionId)
+        {
+            lock (_connections)
+            {
+                HashSet<string> connections;
+                if (!_connections.TryGetValue(key, out connections))
+                {
+                    return;
+                }
+
+                lock (connections)
+                {
+                    connections.Remove(connectionId);
+
+                    if (connections.Count == 0)
+                    {
+                        _connections.Remove(key);
+                    }
+                }
+            }
+        }
+
+        public void RemoveByConnectionId(string connectionId)
+        {
+            lock (_connections)
+            {
+                Tuple<T, HashSet<string>> connection = null;
+                foreach (var kv in _connections)
+                {
+                    if (kv.Value.Contains(connectionId))
+                        connection = Tuple.Create(kv.Key, kv.Value);
+                }
+                if (connection == null) return;
+
+                lock (connection.Item2)
+                {
+                    connection.Item2.Remove(connectionId);
+
+                    if (connection.Item2.Count == 0)
+                    {
+                        _connections.Remove(connection.Item1);
+                    }
+                }
+            }
+        }
+    }
+}
